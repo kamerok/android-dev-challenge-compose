9a6112e141c6d58474a647b3c0f2cf9e0268b03b
Maksim Dvoretskii <kamerok@gmail.com>
2021-02-28 16:33:50 +0300

    Format project

--- app/src/main/java/com/example/androiddevchallenge/pet/Pet.kt
+++ app/src/main/java/com/example/androiddevchallenge/pet/Pet.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.androiddevchallenge.pet
 
 import androidx.compose.foundation.background
@@ -22,7 +37,6 @@ import com.example.androiddevchallenge.R
 import com.example.androiddevchallenge.domain.PetRepository
 import com.example.androiddevchallenge.ui.theme.MyTheme
 import com.example.androiddevchallenge.viewModelFactory
-
 
 @Composable
 fun PetScreen(id: String, repository: PetRepository, setupAppBar: (String, Boolean) -> Unit) {
--- app/src/main/java/com/example/androiddevchallenge/petlist/PetListViewModel.kt
+++ app/src/main/java/com/example/androiddevchallenge/petlist/PetListViewModel.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.androiddevchallenge.petlist
 
 import androidx.lifecycle.ViewModel
@@ -5,9 +20,7 @@ import com.example.androiddevchallenge.domain.Pet
 import com.example.androiddevchallenge.domain.PetRepository
 import kotlinx.coroutines.flow.StateFlow
 
-
 class PetListViewModel(repository: PetRepository) : ViewModel() {
 
     val state: StateFlow<List<Pet>> = repository.getPets()
-
 }
